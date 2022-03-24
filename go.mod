b648208a9d99512f6f155a5de21ae7ca95b8821c
Jose Manuel Garcia <manuelobezo@gmail.com>
2022-03-23 19:58:26 -0600

    CORS fixed

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-chi/chi v4.0.4+incompatible
+	github.com/go-chi/cors v1.2.0
 	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.4
 	github.com/orlmonteverde/go-postgres-microblog v1.4.0
--- internal/server/server.go
+++ internal/server/server.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/http"
 	"time"
-
+	"github.com/go-chi/cors"
 	"github.com/go-chi/chi"
     "github.com/go-chi/chi/middleware"
 	v1 "github.com/manuelobezo/go-postgres-ambertAlert/internal/server/v1"
@@ -20,7 +20,15 @@ type Server struct {
 func New(port string) (*Server, error) {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-    r.Use(middleware.Recoverer)
+	r.Use(middleware.Recoverer)
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+	r.Use(cors.Handler)
 
 	// API routes version 1.
 	r.Mount("/api/v1", v1.New())
